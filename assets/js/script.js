8d5cb2e5684bb6d627c591448c38b3e84f541355
Javier Vergara <ja.vergarac96@gmail.com>
2024-02-22 22:16:45 -0300

    implementacion grafico pastel y carga tarjeta

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,26 +14,52 @@ $(document).ready(function () {
             $('#tarjeta').show();
 
             $.ajax({
-                url: `https://superheroapi.com/api/3387770791513256/${codHero}`,
+                url: `https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/3387770791513256/${codHero}`,
                 type: 'GET',
-                success: function(response) {
+                success: function (response) {
                     console.log("Objeto response: ", response)
 
+                    $('#imagen').attr('src', response.image.url);
                     $('#nombreHero').text(response.name);
+                    $('#conexiones').text(response.connections['group-affiliation']);
+                    $('#publicadoPor').text(response.biography.publisher);
+                    $('#ocupacionHero').text(response.work.occupation);
+                    $('#primeraAparicion').text(response.biography['first-appearance']);
+                    $('#alturaHero').text(response.appearance.height);
+                    $('#pesoHero').text(response.appearance.weight);
+                    $('#alianzas').text(response.biography.aliases);
 
-                    // $('#imagen').attr('src', response.image.url);
-                    // $('#nombreHero').text(response.name);
-                    // $('#conexiones').text(response.connections['group-affiliation']);
-                    // $('#publicadoPor').text(response.biography.publisher);
-                    // $('#ocupacionHero').text(response.work.occupation);
-                    // $('#primeraAparicion').text(response.biography['first-appearance']);
-                    // $('#alturaHero').text(response.appearance.height);
-                    // $('#pesoHero').text(response.appearance.weight);
-                    // $('#alianzas').text(response.biography.aliases);
+                    var chart = new CanvasJS.Chart("chartContainer", {
+                        theme: "light2",
+                        exportEnabled: true,
+                        animationEnabled: true,
+                        title: {
+                            text: "Estadisticas de Poder"
+                        },
+                        data: [{
+                            type: "pie",
+                            startAngle: 25,
+                            toolTipContent: "<b>{label}</b>: {y}%",
+                            showInLegend: "true",
+                            legendText: "{label}",
+                            indexLabelFontSize: 12,
+                            indexLabel: "{label} - {y}%",
+                            dataPoints: [
+                                { y: parseInt(response.powerstats.power), label: "power" },
+                                { y: parseInt(response.powerstats.speed), label: "speed" },
+                                { y: parseInt(response.powerstats.strength), label: "strength" },
+                                { y: parseInt(response.powerstats.combat), label: "combat" },
+                                { y: parseInt(response.powerstats.intelligence), label: "intelligence" },
+                                { y: parseInt(response.powerstats.durability), label: "durability" }
+                            ]
+                        }]
+                    });
 
-                },    
+                    chart.render();
+
+                },
                 dataType: 'json',
-                error: function(error) {
+                error: function (error) {
                     console.log("Objeto error: ", error)
                     console.error('Error al obtener los datos:', error.status);
                 }
